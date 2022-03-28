9d1a364adb4b847dba7ce0b03f81df0cb5a0e631
Scott <55364794+scottroot@users.noreply.github.com>
2022-03-28 09:47:34 -0400

    fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
-import subprocess
+# import subprocess
+from uri_request import whatTheFile
 
 
 app = Flask(__name__)
@@ -19,8 +20,9 @@ class status (Resource):
 
 class fileurl(Resource):
     def get(self, url):
-        filetype = subprocess.check_output(['python', 'uri_request.py', url])
-        filetype = filetype.decode('UTF-8').strip()
+        # filetype = subprocess.check_output(['python', 'uri_request.py', url])
+        # filetype = filetype.decode('UTF-8').strip()
+        filetype = whatTheFile(url)
         return jsonify({'data': filetype})
 
 
--- uri_request.py
+++ uri_request.py
@@ -1,20 +1,24 @@
-import magic
 from urllib.parse import unquote
 import requests
-import sys
+
+def magicId (content):
+    from magic_lib import Magic
+    m = Magic()
+    return m.from_buffer(content)
 
 def whatTheFile (uri):
     uri = unquote(str(uri))
     uri = "https://arweave.net/" + uri
     data = requests.get(uri)
-    dtext = data.text
-    # m = magic.Magic()
-    # found = m.from_buffer(data.content)
-    return magic.from_buffer(data.content)
-# print(whatTheFile("bfWnri0wv_Hw-qFldml8OtBmJl-SoCwVJtuaN90sxTk"))
+    # dtext = data.text
+    # print(dtext)
+    return magicId(data.content)
 
-try:
-    uri = sys.argv[1]
-    print(whatTheFile(uri))
-except:
-    pass
+# try:
+#     uri = xsys.argv[1]
+#     print(whatTheFile(uri))
+# except:
+#     print(whatTheFile('-UUtpZh3_A0VBnnU9fP0yon-xFV26d5uarXE-xMHMqY'))
+#     pass
+
+# print(whatTheFile('-UUtpZh3_A0VBnnU9fP0yon-xFV26d5uarXE-xMHMqY'))
