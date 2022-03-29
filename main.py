15a544a739092d482b2f0822d48df347d2f415b8
Scott <55364794+scottroot@users.noreply.github.com>
2022-03-28 23:17:28 -0400

    fix main

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class status (Resource):
 class fileurl(Resource):
     def get(self):
         response = runArweaveAPI()
-        return jsonify({response})
+        return jsonify({"data": response})
 
 
 api.add_resource(status, '/')
